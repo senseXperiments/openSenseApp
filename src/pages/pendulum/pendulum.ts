d831e08227a744e39d6e58a9c8198427ab8f8e3c
Benjamin Karic <b_kari02@uni-muenster.de>
2019-01-23 10:09:45 +0100

    Update pendulum.ts

--- src/pages/pendulum/pendulum.ts
+++ src/pages/pendulum/pendulum.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import * as HighCharts from 'highcharts';
 
 /**
  * Generated class for the PendulumPage page.
@@ -20,6 +21,28 @@ export class PendulumPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PendulumPage');
+    
+    HighCharts.chart('xValues', {
+      chart: {
+        type: 'spline'
+      },
+      title: {
+        text: 'Acceleration on X-Axis'
+      },
+      xAxis: {
+        title: {
+          text: 'Measurements'
+        }
+      },
+      yAxis: {
+        title: {
+          text: 'Value of Acceleration'
+        }
+      },
+      series: [{
+        data: [9, 6, 9, 8, 1, 6, 10, 4, 1, 3, 2, 3, 12, 8, 10, 1, 3, 12, 8, 10, 4, 5, 4, 7, 0, 7, 7, 12, 1, 3, 0, 5, 3, 8, 7, 6, 5, 11, 3, 6, 8, 7, 5, 2, 0, 5, 2, 9, 9, 4]
+        }]
+    });
   }
 
 }
