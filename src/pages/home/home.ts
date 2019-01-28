5ff476febff4623075a3b93938dafb1bf496e2e4
Christopher R <rohti147@googlemail.com>
2019-01-28 20:49:25 +0100

    updated the Home page
    Short overview of the capabilities in our App

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MaxaccPage } from '../maxacc/maxacc';
+import { AccelerometerPage } from '../accelerometer/accelerometer';
+import { GyroscopePage } from '../gyroscope/gyroscope';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +14,17 @@ export class HomePage {
 
   }
 
+  //Buttons
+  //Button to get to Accelerometer Page
+  btnClickAccel() {
+    this.navCtrl.push(AccelerometerPage);
+  }
+  //Button to get to the Game
+  btnClickGame() {
+    this.navCtrl.push(MaxaccPage);
+  }
+  //Button to get to the Gyroscope
+  btnClickGyros() {
+    this.navCtrl.push(GyroscopePage);
+  }
 }
